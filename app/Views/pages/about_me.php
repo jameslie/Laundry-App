ffcb1fb7d200a38f81ca8fdf28487be0788f7fce
jameslie <bonjames413@gmail.com>
2021-01-12 16:39:12 +0700

    coba

--- app/Views/pages/about_me.php
+++ app/Views/pages/about_me.php
@@ -1,6 +1,84 @@
 <?= $this->extend('layout/template') ?>
 <?= $this->section('content') ?>
 <div class="container">
-    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias optio quas ducimus hic, quae aliquam quidem aperiam dicta sit. Fugit sapiente quaerat ipsum iste odit veritatis cumque aut eligendi eaque!</h1>
+    <h3>About Us</h3>
+    <div class="tittle_head_w3layouts">
+        <h4>Powerful Features</h4>
+
+        <P>Our services are not only fast, clean, efficient, they are also very reliable. Our customer care executives are especially trained to provide guidance and ensure on-time delivery.</p>
+    </div>
+    <div class="inner_sec_info_agileits_w3">
+        <!-- <div class="border1"> -->
+        <div class="w3_banup">
+            <div class="col-md-4 w3_ret">
+                <div class="col-md-10 w3_txt">
+                    <h4>Wash </h4>
+                    <p>Our services are not only fast, clean, efficient, they are also very reliable. </p>
+                </div>
+                <div class="col-md-2 w3ls_ic">
+                    <i class="fa fa-crosshairs" aria-hidden="true"></i>
+
+                </div>
+
+                <div class="clearfix"></div>
+
+                <div class="col-md-10 w3_txt">
+
+                    <h4>Dry</h4>
+                    <p>Our services are not only fast, clean, efficient, they are also very reliable. </p>
+                </div>
+                <div class="col-md-2 w3ls_ic">
+                    <i class="fa fa-assistive-listening-systems" aria-hidden="true"></i>
+                </div>
+                <div class="clearfix"></div>
+
+                <div class="col-md-10 w3_txt">
+
+                    <h4>Iron</h4>
+                    <p>Our services are not only fast, clean, efficient, they are also very reliable. .</p>
+                </div>
+                <div class="col-md-2 w3ls_ic">
+                    <i class="fa fa-tint" aria-hidden="true"></i>
+                </div>
+                <div class="clearfix"></div>
+
+            </div>
+            <div class="col-md-4 w3l_mid">
+                <div class="bulb">
+                    <img src="images/pic.jpg" alt="" />
+                </div>
+            </div>
+            <div class="col-md-4 wthree_r">
+                <div class="col-md-2 w3ls_ic">
+                    <i class="fa fa-street-view" aria-hidden="true"></i>
+                </div>
+                <div class="col-md-10 w3_txt">
+                    <h4>Bedding</h4>
+                    <p class="move">Our services are not only fast, clean, efficient, they are also very reliable. </p>
+                </div>
+                <div class="clearfix"></div>
+
+                <div class="col-md-2 w3ls_icr">
+                    <i class="fa fa-fast-forward" aria-hidden="true"></i>
+                </div>
+                <div class="col-md-10 w3_txt">
+                    <h4>Speedy</h4>
+                    <p class="move">Our services are not only fast, clean, efficient, they are also very reliable. </p>
+                </div>
+                <div class="clearfix"></div>
+
+                <div class="col-md-2 w3ls_icr">
+                    <i class="fa fa-bolt" aria-hidden="true"></i>
+                </div>
+                <div class="col-md-10 w3_txt">
+                    <h4>Stream</h4>
+                    <p class="move">Our services are not only fast, clean, efficient, they are also very reliable. </p>
+                </div>
+                <div class="clearfix"></div>
+            </div>
+
+            <div class="clearfix"></div>
+        </div>
+    </div>
+    <!-- </div> -->
 </div>
-<?= $this->endsection() ?>
